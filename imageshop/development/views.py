dbca6628b8f0fbdf15f2e0d26cce6522129b0bab
tb <tb@tbs-MacBook-Pro.local>
2017-10-26 19:47:12 +0100

    Register view implemented.

--- imageshop/development/views.py
+++ imageshop/development/views.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
-#own imports
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("This is our goddamn landing page.")
+
+def signup(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.isvalid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			logn(request,user)
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	return render(request, 'signup.html', {'form':form})
