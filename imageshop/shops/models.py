9324f8d7a8e3c9e76c5334d9bb8de976e70c4196
warlockerino <s0545655@htw-berlin.de>
2017-10-27 15:11:15 +0200

    basic infrastructure for Shops

--- imageshop/shops/models.py
+++ imageshop/shops/models.py
@@ -7,5 +7,5 @@ from accounts.models import UserProfile
 class Shop(models.Model):
     owner = models.OneToOneField(UserProfile)
     created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    description = models.CharField(max_length=200, default='%s shop' % self.owner.UserProfile.user.name)
+    description = models.CharField(max_length=200, default='%s shop' % self.owner.UserProfile.user.username)
 
