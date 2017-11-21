cebf45dac258a6766b9bce527e2fbc7303facc46
Muhammed Erek <erekm2012@gmx.de>
2017-11-21 13:57:29 +0100

    View for all photos

--- photo/urls.py
+++ photo/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from photo.views import viewPhoto, createPhoto, updatePhoto
+from photo.views import viewPhoto, createPhoto, updatePhoto, viewAllPhotos
 
 urlpatterns = [
 
-    url(r'^view/(?P<pk>[0-9]+)/$', viewPhoto.as_view(), name='view'),
+    url(r'^view/(?P<pk>[0-9]+)$', viewPhoto.as_view(), name='view'),
+    url(r'^view', viewAllPhotos.as_view(), name='viewAll'),
     url(r'^add$', createPhoto.as_view(), name='upload'),
-    url(r'^edit/(?P<pk>[0-9]+)/$', updatePhoto.as_view(), name='update'),
+    url(r'^edit/(?P<pk>[0-9]+)$', updatePhoto.as_view(), name='update'),
 ]
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -5,9 +6,21 @@ from django.core.exceptions import PermissionDenied
 from django.utils import timezone
 from .models import Photo
 
+
 class viewPhoto(DetailView):
     model = Photo
     template_name = 'photo/photo_detailView.html'
+
+
+class viewAllPhotos(ListView):
+    model = Photo
+    template_name = 'photo/photo_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(viewAllPhotos, self).get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
 
 class createPhoto(LoginRequiredMixin, CreateView):
     model = Photo
@@ -19,6 +32,7 @@ class createPhoto(LoginRequiredMixin, CreateView):
         form.instance.created = timezone.now()
         form.instance.last_modified = timezone.now()
         return super(createPhoto, self).form_valid(form)
+
 
 class updatePhoto(LoginRequiredMixin, UpdateView):
     model = Photo
