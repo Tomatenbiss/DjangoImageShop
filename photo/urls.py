1f60cccfed5654abef7bb15981af5ea7cd4c7121
OHDMax <max@ugschulze.de>
2017-11-22 14:05:49 +0100

    Correct URL  pattern for photo addition

--- photo/urls.py
+++ photo/urls.py
@@ -4,8 +4,8 @@ from photo.views import viewPhoto, createPhoto, updatePhoto, viewAllPhotos
 
 urlpatterns = [
 
-    url(r'^view/(?P<pk>[0-9]+)$', viewPhoto.as_view(), name='view'),
+    url(r'^view/(?P<pk>[0-9]+)/$', viewPhoto.as_view(), name='view'),
     url(r'^view', viewAllPhotos.as_view(), name='viewAll'),
-    url(r'^add$', createPhoto.as_view(), name='upload'),
-    url(r'^edit/(?P<pk>[0-9]+)$', updatePhoto.as_view(), name='update'),
+    url(r'^add', createPhoto.as_view(), name='upload'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', updatePhoto.as_view(), name='update'),
 ]
