1c6e9f3d3f86a54c4367ab57aed39eda5b5a4f76
OHDMax <max@ugschulze.de>
2017-11-22 18:03:40 +0100

    Fix case where PhotoList was shown instead of PhotoDetail

--- photo/urls.py
+++ photo/urls.py
@@ -5,7 +5,7 @@ from photo.views import viewPhoto, createPhoto, updatePhoto, viewAllPhotos
 urlpatterns = [
 
     url(r'^view/(?P<pk>[0-9]+)/$', viewPhoto.as_view(), name='view'),
-    url(r'^view', viewAllPhotos.as_view(), name='viewAll'),
-    url(r'^add', createPhoto.as_view(), name='upload'),
+    url(r'^view/$', viewAllPhotos.as_view(), name='viewAll'),
+    url(r'^add/$', createPhoto.as_view(), name='upload'),
     url(r'^edit/(?P<pk>[0-9]+)/$', updatePhoto.as_view(), name='update'),
 ]
