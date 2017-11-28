156b676cac64f9495168a68cb2963fa4dc0e1d89
root <lukas.gruenewald@yourhero.de>
2017-11-28 16:42:32 +0100

    Fix: User age calculation in profile

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User, Group
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from datetime import date
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
@@ -13,22 +11,16 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
 
+    def calculate_age(self):
+        import datetime
+        return int((datetime.date.today() - self.birth_date).days / 365.25  )
+        age = property(calculate_age)
+
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
-
-def age(self):
-            dob = self.birth_date
-            tod = datetime.date.today()
-            my_age = (tod.year - dob.year) - int((tod.month, tod.day) < (dob.month, dob.day))
-            return my_age
-
-#def calculate_age(born):
-#    today = date.today()
-#    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-
 
 class Photographer(Profile):
     profile = models.OneToOneField(Profile)
