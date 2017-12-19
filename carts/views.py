db65c1dc6b41f7160e89d8d855369db0e81d90bf
root <lukas.gruenewald@yourhero.de>
2017-12-19 14:47:18 +0100

    Adds redirect to shopping cart

--- carts/views.py
+++ carts/views.py
@@ -11,19 +11,18 @@ def add(request):
      return HttpResponse("Photo already in shopping cart")
     else:
      cart.add(phot, price=phot.price)
-     return HttpResponse("Added")
-    
+     return render(request, 'carts/redirect.html')
 
 def remove(request):
     cart = Cart(request.session)
     phot = Photo.objects.get(id=request.GET.get('id'))
     cart.remove(phot)
-    return HttpResponse("Removed")
+    return render(request, 'carts/redirect.html')
 
 def clear(request):
     cart = Cart(request.session)
     cart.clear()
-    return HttpResponse("Cleared")
+    return render(request, 'carts/redirect.html')
 
 def show(request):
     return render(request, 'carts/show-cart.html')
