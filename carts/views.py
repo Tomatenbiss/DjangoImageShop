8ed9e09a3d64c0a95a64ec44da63606071d8c5bf
Muhammed Erek <erekm2012@gmx.de>
2017-12-27 23:05:26 +0100

    Add Badge for the amount of items in cart

--- carts/views.py
+++ carts/views.py
@@ -4,14 +4,16 @@ from django.shortcuts import render
 from carton.cart import Cart
 from photos.models import Photo
 
+
 def add(request):
     cart = Cart(request.session)
     phot = Photo.objects.get(id=request.GET.get('id'))
     if phot in cart.products:
-     return HttpResponse("Photo already in shopping cart")
+        return HttpResponse("Photo already in shopping cart")
     else:
-     cart.add(phot, price=phot.price)
-     return render(request, 'carts/redirect.html')
+        cart.add(phot, price=phot.price)
+        return render(request, 'carts/redirect.html')
+
 
 def remove(request):
     cart = Cart(request.session)
@@ -19,10 +21,12 @@ def remove(request):
     cart.remove(phot)
     return render(request, 'carts/redirect.html')
 
+
 def clear(request):
     cart = Cart(request.session)
     cart.clear()
     return render(request, 'carts/redirect.html')
+
 
 def show(request):
     return render(request, 'carts/show-cart.html')
