ecfa824d02d9435f28aae2d25ff241a2c6a451d5
root <lukas.gruenewald@yourhero.de>
2018-01-16 13:57:46 +0100

    Adds photoseries to shoppingcarts

--- carts/urls.py
+++ carts/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url, include
 
-from carts.views import add, clear, remove, show, add_series, remove_series
+from carts.views import add, clear, remove, show
 
 urlpatterns = [
     url(r'^add/$', add, name='add'),
-    url(r'^add_series/$', add_series, name='add_series'),
     url(r'^clear/$', clear, name='clear'),
     url(r'^remove/$', remove, name='remove'),
-    url(r'^remove_series/$', remove_series, name='remove_series'),
     url(r'^show/$', show, name='show'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -7,33 +7,29 @@ from photoseries.models import Photoseries
 
 def add(request):
     cart = Cart(request.session)
-    phot = Photo.objects.get(id=request.GET.get('id'))
-    if phot in cart.products:
-        return HttpResponse("Photo already in shopping cart")
-    else:
-        cart.add(phot, price=phot.price)
+    try:
+        photseries = Photoseries.objects.get(id=request.GET.get('id'))
+        photss = photseries.images.all()
+        for phots in photss:
+            #print "object is a photoseries"
+            if phots in cart.products:
+               cart.remove(phots)
+               cart.add(phots, price=photseries.price)
+            else:
+               cart.add(phots, price=photseries.price)
         return render(request, 'carts/redirect.html')
-
-def add_series(request):
-    cart = Cart(request.session)
-    photoseries = Photoseries.objects.get(id=request.GET.get('id'))
-    if photoseries in cart.products:
-        return HttpResponse("Photoseries is already in shopping cart")
-    else:
-        cart.add(photoseries, price=photoseries.price)
-        return render(request, 'carts/redirect.html')
-
+    except:
+        phot = Photo.objects.get(id=request.GET.get('id'))
+        if phot in cart.products:
+            return HttpResponse("Photo already in shopping cart")
+        else:
+            cart.add(phot, price=phot.price)
+            return render(request, 'carts/redirect.html')
 
 def remove(request):
     cart = Cart(request.session)
     phot = Photo.objects.get(id=request.GET.get('id'))
     cart.remove(phot)
-    return render(request, 'carts/redirect.html')
-
-def remove_series(request):
-    cart = Cart(request.session)
-    photoseries = Photoseries.objects.get(id=request.GET.get('id'))
-    cart.remove(photoseries)
     return render(request, 'carts/redirect.html')
 
 def clear(request):
