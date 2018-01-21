daea4086c7da0a03eed56b5bcf2a1551dfea25aa
OHDMax <max@ugschulze.de>
2018-01-21 21:45:35 +0100

    \#61: Add order_list, order_detailView and open_Order views

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from orders.views import viewMyOrders, viewOrder, viewMyOpenOrders
 
 urlpatterns = [
-
+    url(r'^my/$', viewMyOrders.as_view(), name='myOrders'),
+    url(r'^view/(?P<pk>[0-9]+)/$', viewOrder.as_view(), name='viewOrder'),
+    url(r'^open/$', viewMyOpenOrders.as_view(), name='myOpenOrders'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+
+from orders.models import Order
 
 # Create your views here.
+
+class viewMyOrders(LoginRequiredMixin, ListView):
+    model = Order
+    template_name = 'orders/order_list.html'
+
+    def get_queryset(self):
+        return Order.objects.filter(buyer=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super(viewMyOrders, self).get_context_data(**kwargs)
+        if self.request.user.groups.filter(name='photographer').exists():
+             context['orders_as_seller'] = Order.objects.filter(seller=self.request.user)
+        return context
+
+
+class viewOrder(LoginRequiredMixin, DetailView):
+    model = Order
+    template_name = 'orders/order_detailView.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(viewOrder, self).get_context_data(**kwargs)
+        if (self.request.user != context['object'].seller and self.request.user != context['object'].buyer):
+            raise PermissionDenied
+        return context
+
+
+class viewMyOpenOrders(LoginRequiredMixin, ListView):
+    model = Order
+    template_name = 'orders/order_list.html'
+
+    def get_queryset(self):
+        return Order.objects.filter(seller=self.request.user, paid=False)
+    
+    def get_context_data(self, **kwargs):
+        context = super(viewMyOpenOrders, self).get_context_data(**kwargs)
+        if not self.request.user.groups.filter(name='photographer').exists():
+             raise PermissionDenied
+        return context
