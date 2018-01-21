7f35546e491e8d79fc1440e68fd18b753c318a6f
OHDMax <max@ugschulze.de>
2018-01-21 13:39:10 +0100

    \#61: Remove obsolete cart implementation

--- carts/models.py
+++ carts/models.py
@@ -1,15 +1,2 @@
 from django.db          import models
-from photos.models       import Photo
-from photoseries.models import Photoseries
 
-from django.contrib.auth.models     import User
-
-class Cart(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    #Entweder many2many oder lsite mit bild ids
-    images = models.ManyToManyField(Photo)
-    #images = []
-    #add methoden etc? spart eine Tabelle
-    series = models.ManyToManyField(Photoseries) 
-    sent = False
-    payed = False
