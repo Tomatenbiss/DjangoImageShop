6afa32d71e166f5e8a0a6bb50ecd88ba76af9115
warlockerino <s0545655@htw-berlin.de>
2018-01-21 18:44:19 +0100

    new views and swag

--- photoseries/urls.py
+++ photoseries/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import url
 
-from .views import createPhotoseries, upload_done, upload_photos, viewPhotoseries, viewOwnPhotoseries
+from .views import  (createPhotoseries, 
+                    upload_done, 
+                    upload_photos,  
+                    viewPhotoseries, 
+                    viewOwnPhotoseries, 
+                    updatePhotoseries,
+                    deletePhotoseries)
+
 from .forms import PhotoSeriesForm
 urlpatterns = [
     # view photoseries
    url(r'^view/(?P<pk>[0-9]+)/$', viewPhotoseries.as_view(), name='view_series'),
+   url(r'^thumb/(?P<pk>[0-9]+)/$', viewPhotoseries.as_view(), name='view_series'),
+
     # upload from existing images
     url(r'^add/$', createPhotoseries.as_view(), name='upload_series'),
     # uplaod new images into series
@@ -12,4 +21,8 @@ urlpatterns = [
     # success url for the upload -- deprecated soon
     url(r'^adds_done/', upload_done, name='upload_done'),
     url(r'^owned/$', viewOwnPhotoseries.as_view(), name='viewOwn_series'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', updatePhotoseries.as_view(), name='update'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', deletePhotoseries.as_view(), name='delete'),
+
+
 ]
--- photoseries/views.py
+++ photoseries/views.py
@@ -1,24 +1,42 @@
-from django.shortcuts import render, redirect, render_to_response
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.detail import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Photoseries
-from photos.models import Photo
-from django.utils import timezone
+from django.shortcuts               import (render, 
+                                            redirect, 
+                                            render_to_response)
+from django.views.generic           import ListView
+from django.views.generic.edit      import (CreateView, 
+                                            UpdateView, 
+                                            DeleteView)
+from django.views.generic.detail    import DetailView
+from django.contrib.auth.mixins     import LoginRequiredMixin
+from .models                        import Photoseries
+from photos.models                  import Photo
+from django.utils                   import timezone
+from django.core.exceptions         import PermissionDenied
+
+
+class OwnerRequiredView(object):
+    
+    def get_context_data(self, **kwargs):
+        context = super(OwnerRequiredView, self).get_context_data(**kwargs)
+        if context['object'].owner != self.request.user:
+            raise PermissionDenied
+        return context
 
 # form for photoseries
 class createPhotoseries(LoginRequiredMixin, CreateView):
     model = Photoseries
     # fields that can be edited
-    fields = ['title', 'description', 'images', 'price']
+    fields = ['title', 
+                'description', 
+                'images', 
+                'price', 
+                'public']
     template_name = 'photoseries/photoseries_form.html'
-    
     # set owner based on session and add timestamps
     def form_valid(self, form):
         form.instance.owner = self.request.user
         form.instance.created = timezone.now()
         form.instance.last_modified = timezone.now()
+        form.save()
         return super(createPhotoseries, self).form_valid(form)
 
     # get images from photographer and display them for selection
@@ -34,20 +52,21 @@ def upload_photos(request):
         files = request.FILES.getlist('images')
         description = request.POST['description']
         title = request.POST['title']
+        pub = request.POST['public']
         price = 2.0
-        s_instance = Photoseries(title = title , description = description, owner=request.user ,price = price)
+        s_instance = Photoseries(title = title , description = description, owner=request.user ,price = price, public = pub)
         # save series before adding images
         s_instance.save()
         # iterate over files
         for count,a_file in enumerate(files):
             # create & save Image for every file
-            p_instance = Photo(image=a_file, owner=request.user, public = False)
+            p_instance = Photo(image=a_file, owner=request.user, public = pub)
             p_instance.save()
             # add image to series
             s_instance.images.add(p_instance)
         # save modified series
         s_instance.save()
-        return redirect('upload_done')
+        return redirect('viewPhotoseries')
 
     return render(request, 'photoseries/upload.html')
 
@@ -57,7 +76,7 @@ def upload_done(request):
 
 class viewPhotoseries(DetailView):
     model = Photoseries
-    template_name = 'photoseries/photoseries_thumbnails.html'
+    template_name = 'photoseries/photoseries_thumbnail.html'
 
 class viewOwnPhotoseries(LoginRequiredMixin, ListView):
     model = Photoseries
@@ -66,3 +85,19 @@ class viewOwnPhotoseries(LoginRequiredMixin, ListView):
         '''Only show photos of the current User'''
         return Photoseries.objects.filter(owner=self.request.user)
 
+class updatePhotoseries(LoginRequiredMixin, UpdateView):
+    model = Photoseries
+    fields = ['title', 
+                'description', 
+                'price', 
+                'public', 
+                ]
+    template_name = 'photoseries/photoseries_form.html'
+
+    def form_valid(self, form):
+        form.instance.last_modified = timezone.now()
+        return super(updatePhotoseries, self).form_valid(form)
+
+class deletePhotoseries(LoginRequiredMixin, OwnerRequiredView, DeleteView):
+    model = Photoseries
+    success_url = '/photoseries/list/'
