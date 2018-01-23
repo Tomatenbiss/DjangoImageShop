b53736362f81e4d3a77850251e11a40deb784117
Muhammed Erek <erekm2012@gmx.de>
2018-01-23 23:43:29 +0100

    Beautify search_results

--- search/views.py
+++ search/views.py
@@ -1,3 +1,6 @@
+from itertools import chain
+
+from django.db.models import Q
 from django.views.generic import TemplateView
 from photos.models import Photo, PhotoCategory
 from photoseries.models import Photoseries
@@ -13,10 +16,12 @@ class searchResultView(TemplateView):
         context['keyword'] = keyword
         # Search for photos
         context['photos_found_by_title'] = Photo.objects.filter(title__contains=keyword)
-        context['photos_found_by_description'] = Photo.objects.filter(description__contains=keyword)
+        context['photos_found_by_description'] = Photo.objects.filter(description__contains=keyword).exclude(title__contains=keyword)
         context['categories'] = PhotoCategory.objects.filter(name__contains=keyword)
-        context['photos_found_by_category'] = Photo.objects.filter(categories__in=context['categories'])
+        context['photos_found_by_category'] = Photo.objects.filter(categories__in=context['categories']).exclude(title__contains=keyword, description__contains=keyword)
+        context['photos'] = list(chain(context['photos_found_by_title'], context['photos_found_by_description'], context['photos_found_by_category']))
         # Search for photoseries
         context['photoseries_found_by_title'] = Photoseries.objects.filter(title__contains=keyword)
-        context['photoseries_found_by_description'] = Photoseries.objects.filter(description__contains=keyword)
-        return context;
+        context['photoseries_found_by_description'] = Photoseries.objects.filter(description__contains=keyword).exclude(title__contains=keyword)
+        context['photoseries'] = list(chain(context['photoseries_found_by_title'], context['photoseries_found_by_description']))
+        return context
