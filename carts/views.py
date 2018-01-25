65624bc42712ea5843a02c4448b670645adcf770
root <lukas.gruenewald@yourhero.de>
2018-01-25 12:58:48 +0100

    Adds photos and photoseries to shoppingcart

--- carts/views.py
+++ carts/views.py
@@ -18,39 +18,18 @@ def add(request):
     cart = Cart(request.session)
     try:
         photseries = Photoseries.objects.get(id=request.GET.get('id'))
-        photss = photseries.images.all()
-        iterations = 0
-        for phots in photss:
-           if iterations == 0:
-              if phots in cart.products:
-                 if request.user == phots.owner:
-                     return HttpResponse("You can not buy your own photoseries. Sorry.")
-                 else:
-                     cart.remove(phots)
-                     cart.add(phots, price=photseries.price)
-                     iterations += 1
-              else:
-                 if request.user == phots.owner:
-                     return HttpResponse("You can not buy your own photoseries. Sorry.")
-                 else:
-                     cart.add(phots, price=photseries.price)
-                     iterations += 1
-           else:
-              if request.user == phots.owner:
-                  return HttpResponse("You can not buy your own photoseries. Sorry.")
-              else:
-                  cart.add(phots, 0.0)
+        cart.add(photseries, price=photseries.price)
         return render(request, 'carts/redirect.html')
     except:
         phot = Photo.objects.get(id=request.GET.get('id'))
-        if phot in cart.products:
-            return HttpResponse("Photo already in shopping cart")
-        else:
-            if request.user == phot.owner:
-                return HttpResponse("You can not buy your own photos. Sorry.")
-            else:
-                cart.add(phot, price=phot.price)
-                return render(request, 'carts/redirect.html')
+#        if phot in cart.products:
+#            return HttpResponse("Photo already in shopping cart")
+#        else:
+ #           if request.user == phot.owner:
+  #              return HttpResponse("You can not buy your own photos. Sorry.")
+   #         else:
+        cart.add(phot, price=phot.price)
+        return render(request, 'carts/redirect.html')
 
 def remove(request):
     cart = Cart(request.session)
